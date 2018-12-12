dixudx/tumblr-crawler

commit d6949c77283afd3cdb4a32d51d3487829c94e3c7

Added logging posts contents to enable future extration captions (#109)
* Added logging posts contents to enable future extration of text/captions from xml and json files!
* Make EACH_POST_AS_SEPARATE_JSON false by default

tumblr-photo-video-ripper.py
@@ -25,6 +25,10 @@ MEDIA_NUM = 50
 # Numbers of downloading threads concurrently
 THREADS = 10
 
+# Do you like to dump each post as separate json (otherwise you have to extract from bulk xml files)
+# This option is for convenience for terminal users who would like to query e.g. with ./jq (https://stedolan.github.io/jq/)
+EACH_POST_AS_SEPARATE_JSON = False
+
 
 def video_hd_match():
     hd_pattern = re.compile(r'.*"hdUrl":("([^\s,]*)"|false),')
@@ -196,12 +200,22 @@ class CrawlerScheduler(object):
                 print("Site %s does not exist" % site)
                 break
 
+            response_file = "{0}/{0}_{1}_{2}_{3}.response.xml".format(site, medium_type, MEDIA_NUM, start)
+            with open(response_file, "w") as text_file:
+                text_file.write(response.content)
+
             try:
                 xml_cleaned = re.sub(u'[^\x20-\x7f]+',
                                      u'', response.content.decode('utf-8'))
                 data = xmltodict.parse(xml_cleaned)
                 posts = data["tumblr"]["posts"]["post"]
                 for post in posts:
+		    # by default it is switched to false to generate less files, as anyway you can extract this from bulk xml files.
+                    if EACH_POST_AS_SEPARATE_JSON == True:
+                        post_json_file = "{0}/{0}_post_id_{1}.post.json".format(site, post['@id'])
+                        with open(post_json_file, "w") as text_file:
+                            text_file.write(json.dumps(post))
+
                     try:
                         # if post has photoset, walk into photoset for each photo
                         photoset = post["photoset"]["photo"]

commit 7f9b3c0d4600a42e5a12ce12af9ecccb2f941780

fix #116, #118
This issue is introduced by #109

tumblr-photo-video-ripper.py
@@ -202,7 +202,7 @@ class CrawlerScheduler(object):
 
             response_file = "{0}/{0}_{1}_{2}_{3}.response.xml".format(site, medium_type, MEDIA_NUM, start)
             with open(response_file, "w") as text_file:
-                text_file.write(response.content)
+                text_file.write(response.content.decode('utf-8'))
 
             try:
                 xml_cleaned = re.sub(u'[^\x20-\x7f]+',
@@ -210,8 +210,9 @@ class CrawlerScheduler(object):
                 data = xmltodict.parse(xml_cleaned)
                 posts = data["tumblr"]["posts"]["post"]
                 for post in posts:
-		    # by default it is switched to false to generate less files, as anyway you can extract this from bulk xml files.
-                    if EACH_POST_AS_SEPARATE_JSON == True:
+                    # by default it is switched to false to generate less files,
+                    # as anyway you can extract this from bulk xml files.
+                    if EACH_POST_AS_SEPARATE_JSON:
                         post_json_file = "{0}/{0}_post_id_{1}.post.json".format(site, post['@id'])
                         with open(post_json_file, "w") as text_file:
                             text_file.write(json.dumps(post))